b79fe712482005a936d660d15033d3b3b7018392
Tikam Singh <88304261+tkmkushwah@users.noreply.github.com>
2022-06-02 23:12:10 +0530

    shoping list completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import "./styles.css";
-import React from 'react';
-var shoppingList=['aloo🥔','pudhina🥦','pyaj🧄','lahsun🧅','dhania🥬','bengan🍆','tamatar🍅','mitchi🌶','gajar🥕']
+import React from "react";
+var shoppingList = [
+  "aloo🥔",
+  "pudhina🥦",
+  "pyaj🧄",
+  "lahsun🧅",
+  "dhania🥬",
+  "bengan🍆",
+  "tamatar🍅",
+  "mitchi🌶",
+  "gajar🥕"
+];
 export default function App() {
-  function getBg(index)
-  {
-    if(index%2===0)
-    {
-      return "white"
-    }return "green"
+  function getBg(index) {
+    if (index % 2 === 0) {
+      return "white";
+    }
+    return "green";
   }
-    
-  
   return (
     <div className="App">
       <h1>SHOPPING LIST</h1>
       <ul>
-      {
-        shoppingList.map(function(item,index){
-          return <li key={item} style={{backgroundColor:getBg(index),padding:2 rem}}>{item}</li>
-        })
-      }
-    </ul>
+        {shoppingList.map(function (item, index) {
+          return (
+            <li
+              key={item}
+              style={{ backgroundColor: getBg(index), padding: "1rem" }}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
