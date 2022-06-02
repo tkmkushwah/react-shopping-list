7c37b026819335daa2f29c91e0e81f25398e4465
Tikam Singh <88304261+tkmkushwah@users.noreply.github.com>
2022-06-02 23:28:30 +0530

    fucntioning

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ var shoppingList = [
 export default function App() {
   function getBg(index) {
     if (index % 2 === 0) {
-      return "white";
+      return "whi";
     }
     return "green";
+  }
+  function itemListClickhandler(item) {
+    console.log("how dare you to touch my " + item + "😂");
   }
   return (
     <div className="App">
@@ -25,6 +28,7 @@ export default function App() {
         {shoppingList.map(function (item, index) {
           return (
             <li
+              onClick={() => itemListClickhandler(item)}
               key={item}
               style={{ backgroundColor: getBg(index), padding: "1rem" }}
             >
